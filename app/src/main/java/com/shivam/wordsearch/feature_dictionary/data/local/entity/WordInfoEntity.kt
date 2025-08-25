82d83f88a57d0a1f85135b7a3f39d2df58eb6585
shivamsharma <shivamsharma654.ss@gmail.com>
2025-08-25 12:26:01 +0530

    feat: add entities, converters, parsers and db class

---
+++ app/src/main/java/com/shivam/wordsearch/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,24 @@
+package com.shivam.wordsearch.feature_dictionary.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.shivam.wordsearch.feature_dictionary.domain.model.Meaning
+import com.shivam.wordsearch.feature_dictionary.domain.model.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val origin: String,
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            meanings = meanings,
+            word = word,
+            sourceUrl = origin,
+            phonetic = phonetic
+        )
+    }
+}
